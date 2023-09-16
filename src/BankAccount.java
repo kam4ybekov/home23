7be8f650f77bd226252728d17bc186cda05b8ffc
kam4ybekov <141447740+kam4ybekov@users.noreply.github.com>
2023-09-16 11:33:53 +0600

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+    private double amount;
+
+    public BankAccount(double initialAmount) {
+        this.amount = initialAmount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double sum) {
+        if (sum > 0) {
+            amount += sum;
+            System.out.println("Вы внесли " + sum + " на свой счет. Новый баланс: " + amount);
+        } else {
+            System.out.println("Неверная сумма для внесения.");
+        }
+    }
+
+    public void withdraw(double sum) throws LimitException {
+        if (sum > 0) {
+            if (sum <= amount) {
+                amount -= sum;
+                System.out.println("Dы сняли " + sum + " со своего счета. Новый баланс: " + amount);
+            } else {
+                throw new LimitException("Недостаточно средств на счете.", amount);
+            }
+        } else {
+            System.out.println("Неверная сумма для снятия. ");
+        }
+    }
 }
+
